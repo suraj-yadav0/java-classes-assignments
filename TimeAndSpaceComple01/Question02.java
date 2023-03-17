0910c12f3afafbf20aff281792a2cc451715b34a
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2023-03-16 23:58:03 -0700

    Add files via upload

---
+++ TimeAndSpaceComple01/Question02.java
@@ -0,0 +1,12 @@
+package TimeAndSpaceComple01;
+
+public class Question02 {
+//    Q2. What is the time complexity of the following code snippet?
+//    int a = 0;
+//for (i = 0; i < N; i++) {
+//        for (j = N; j > i; j--) {
+//            a = a + i + j;
+//        }
+//    }
+//    Answer: O(N*N)
+}
